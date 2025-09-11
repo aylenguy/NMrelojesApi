e83fbb25dc5e692614c74715a51e4206bbf6d821
Aylen Guy <ayluguy@gmail.com>
2025-09-11 19:10:44 -0300

    mas detalles

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -42,9 +42,11 @@ builder.Services.AddCors(options =>
     {
         policy
             .WithOrigins(
-                "https://nm-relojes-buauzen73-aylens-projects-7a096c01.vercel.app", // ✅ Front en Vercel
+                "https://nm-relojes-83zqib2bm-aylens-projects-7a096c01.vercel.app", // ✅ Front en Vercel
                 "http://localhost:5173" // ✅ Para desarrollo local
-   
+                
+
+
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
