ad7af42811e09987fe86c997ccc727e833aa0383
Aylen Guy <ayluguy@gmail.com>
2025-09-25 18:58:29 -0300

    mas cambios

--- src/Infrastructure/Migrations/20250925215807_InitialCreate.Designer.cs
+++ src/Infrastructure/Migrations/20250925215807_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    [Migration("20250925214958_InitialCreate")]
+    [Migration("20250925215807_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
